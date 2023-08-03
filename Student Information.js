a46d89f8ffe1831da4f6ee1ba235156809535a66
Strahil Hristov <strahil1998@abv.bg>
2023-08-03 08:52:08 +0300

    student info

---
+++ Student Information.js
@@ -0,0 +1,5 @@
+function studentInfomation(studentName, studentAge, studentGrade) {
+
+    console.log(`Name: ${studentName}, Age: ${studentAge}, Grade: ${studentGrade.toFixed(2)}`);
+}
+studentInfomation('John', 15, 5.54678)
