09153400e9c1532a981ca172a9b5fd7564fb02d1
Strahil Hristov <strahil1998@abv.bg>
2023-08-23 17:25:57 +0300

    distance

--- Distance Between Points.js
+++ Distance Between Points.js
@@ -1,6 +1,6 @@
-function distance(X1, Y1, X2, Y2){
+function distance(X1, Y1, X2, Y2) {
 
-    let a = Math.abs(X1-X2);
+    let a = Math.abs(X1 - X2);
     let b = Math.abs(Y1 - Y2);
     let distancePoint = Math.sqrt((a * a) + (b * b));
 
